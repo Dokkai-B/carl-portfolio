f8c74915ecb0e8727e18c14d84b2643a514ceb5a
Carl <xix.carlaguas.xix@gmail.com>
2024-09-05 20:13:36 +0800

    Added page files

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -27,14 +27,17 @@ const links = [
 ];
 
 const Nav = () => {
-  return (
-    <nav className="flex gap-8">
-        {links.map((link, index) => {
-            return <Link href ={link.path} key={index}>
-                {link.name}
-            </Link>
-        })}
-    </nav>
+    const pathname = usePathname();
+    console.log(pathname);
+    return (
+        <nav className="flex gap-8">
+            {links.map((link, index) => {
+                return <Link href ={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}
+                >
+                    {link.name}
+                </Link>
+            })}
+        </nav>
   )
 }
 
