e90b2779a3399e3bbd1a7837b90f09a49a7f4a95
Carl <xix.carlaguas.xix@gmail.com>
2024-09-09 05:02:36 +0800

    Fixed apostrophe error.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -100,7 +100,7 @@ const Contact = () => {
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#243546] rounded-xl" onSubmit={handleSubmit}>
               <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
-              <p>  I&apos;m always excited to connect and collaborate on new projects. Whether you need a website, an app, or creative design solutions, I&apos;m here to help. Let's create something great together!
+              <p>  I&apos;m always excited to connect and collaborate on new projects. Whether you need a website, an app, or creative design solutions, I&apos;m here to help. Let&apos;s create something great together!
               </p>
               {/* Input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
