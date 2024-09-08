42e9136591439db0e0939a21f423097801efe8e4
Carl <xix.carlaguas.xix@gmail.com>
2024-09-09 04:38:56 +0800

    Added contact message.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -100,7 +100,8 @@ const Contact = () => {
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#243546] rounded-xl" onSubmit={handleSubmit}>
               <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+              <p>  I&apos;m always excited to connect and collaborate on new projects. Whether you need a website, an app, or creative design solutions, I&apos;m here to help. Let's create something great together!
+              </p>
               {/* Input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input name="firstName" value={formData.firstName} onChange={handleChange} placeholder="Firstname" />
