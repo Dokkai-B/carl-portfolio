d7236f5b359aa7cad1a76cd730c2dcedf243b89e
Carl <xix.carlaguas.xix@gmail.com>
2024-09-09 04:35:13 +0800

    Added Download CV and resume file.

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
 // Components
-import Social from "@/components/Social"
+import Social from "@/components/Social";
 import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
 
@@ -17,23 +16,25 @@ const page = () => {
             <h1 className="h1 mb-6">
               Hello I&apos;m <br /> <span className="text-accent">Carl Patrick Adrian Aguas</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
             </p>
             {/* Button and Socials*/}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button 
-                variant="outline" 
-                size="lg" 
-                className="uppercase flex items-center gap-2"
+              <a
+                href="/assets/cv/Carl_Aguas_Resume.pdf"
+                download
+                className="uppercase flex items-center gap-2 bg-transparent border-2 border-accent text-accent px-6 py-3 rounded-full hover:bg-accent hover:text-primary transition duration-500"
               >
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
-              </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
-                <Social 
-                  containerStyles="flex gap-6" 
+                <Social
+                  containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition all duration-500"
-                  />
+                />
               </div>
             </div>
           </div>
@@ -45,7 +46,7 @@ const page = () => {
       </div>
       <Stats />
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
